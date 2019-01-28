1736d0ad328bcd4832692a698bbd624366a55cbd
koylyakandriy <koylyakandriy@gmail.com>
2019-01-28 21:56:45 +0200

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://koylyak:Djcamp15121990@ds113855.mlab.com:13855/storybooks-prod',
+  googleClientID: '283678357745-llceits22gd6s8n02qsmthp85m9k5sg5.apps.googleusercontent.com',
+  googleClientSecret: 'CzAKuZAlupBW2_fCiy-ySJ_e'
+};
