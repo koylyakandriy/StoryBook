f0b81d6e6e495da100c318723cb04ed37a3e939c
koylyakandriy <koylyakandriy@gmail.com>
2019-02-06 07:48:59 +0200

    all project

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://newstorybook:story123@ds019488.mlab.com:19488/newstorybooks-prod',
-  googleClientID: '283678357745-gcc29ad1d94gr59g0tfv1qcfmndu5u3s.apps.googleusercontent.com',
-  googleClientSecret: 'Ft0wRZMCftyYua4aornhK-sr'
-};
+if(process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
